7187e8c430f55e8156a4f0d6e6a42815cf3c648b
miicosmik <miitstake@gmail.com>
2025-11-02 01:01:47 -0300

    Create heart.py

---
+++ heart.py
@@ -0,0 +1,14 @@
+from aiohttp import web
+import asyncio
+
+async def handle(request):
+    return web.Response(text="Bot está vivo 🐢")
+
+async def start_web_server():
+    app = web.Application()
+    app.router.add_get("/", handle)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, "0.0.0.0", 8080)
+    await site.start()
+    print("[OK] Web service iniciado na porta 8080")
